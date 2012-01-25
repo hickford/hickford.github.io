09c54e158bcc0d1a8eefb40e3bd72f351ffea5de
Matt Hickford <matt.hickford@gmail.com>
2012-01-25 22:04:29 +0000

    CV in txt

---
+++ CV/render-CV.sh
@@ -0,0 +1,6 @@
+#!sh
+pandoc -sS Matthew-Hickford-CV.markdown  -o Matthew-Hickford-CV.html
+markdown2pdf Matthew-Hickford-CV.markdown
+cp Matthew-Hickford-CV.markdown Matthew-Hickford-CV.txt
+chmod 0222 Matthew-Hickford-CV.txt
+attrib +R Matthew-Hickford-CV.txt
